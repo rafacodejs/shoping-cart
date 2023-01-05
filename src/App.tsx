d1b10602ee84b697d6eb713a21285539465b00bb
Rafael Malpica <rafacodejs@gmail.com>
2023-01-05 14:22:10 -0400

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+import Drawer from '@mui/material/Drawer';
+import { LinearProgress } from '@mui/material';
+import Grid from '@mui/material';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import Badge from '@mui/material/Badge';
+import { Wrapper } from './App.styles';
+
+const App = () => {
+  return <div>Hola</div>;
+};
+
+export { App };
